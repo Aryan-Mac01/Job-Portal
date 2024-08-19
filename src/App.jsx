9f04dc3b85726cb6ede9e470eda711dc6ab80baf
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-08-20 01:20:20 +0530

    public

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello, Developer!</h1>
+      <Outlet/>
     </>
   )
 }
