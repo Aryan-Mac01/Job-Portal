5d33f0ee099e8b3ed782e8f94fc893fc32b58352
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-08-25 03:06:46 +0530

    location

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import { useState } from 'react'
 
 const Home = () => {
+  const [query, setQuery] = useState("")
+    const handleInputChange = (event) => {
+        setQuery(event.target.value)
+        //console.log(event.target.value);
+    }
+    console.log(query);
   return (
     <div>
-      <Banner/>
+      <Banner query={query} handleInputChange={handleInputChange}/>
     </div>
   )
 }
